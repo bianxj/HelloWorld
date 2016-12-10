6302645ad035dff5bb5f06a2d5df69b34d672756
bianxj <a_735031002@126.com>
2016-12-10 21:16:29 +0800

    Test

---
+++ app/src/main/java/com/example/administrator/helloworld/Test.java
@@ -0,0 +1,18 @@
+package com.example.administrator.helloworld;
+
+
+/**
+ * Created by Administrator on 2016/11/27.
+ */
+public class Test {
+
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
